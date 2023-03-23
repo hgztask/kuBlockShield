aeb2ee11d018b394cf189cbac4e69442707f4bab
hgztask <79256242+hgztask@users.noreply.github.com>
2023-03-23 12:57:53 +0800

    提交

---
+++ kuBlockShield.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         库街区屏蔽增强器
+// @namespace    http://tampermonkey.net/
+// @license      MIT
+// @version      0.1
+// @description  根据用户名、帖子预览内容关键词、浏览量、评论量、点赞（收藏量进行屏蔽）
+// @author       byhgz
+// @match        *://www.kurobbs.com/*
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// @icon         https://web-static.kurobbs.com/resource/prod/icon.png
+// @grant        none
+// ==/UserScript==
+
+
+
+
+
+(function () {
+    'use strict';
+    console.log("进入库街区")
+    const list = $(".post-list > .item");
+
+
+})
+();
